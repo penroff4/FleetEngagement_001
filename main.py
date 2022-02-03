ff69ccfaee84bed0e92db8a0260f59d627399999
penroff4 <penroff4@gmail.com>
2022-02-02 19:04:34 -0800

    add nations.json, add inputbox.py

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import pygame
 import sys
 import logging
 import pandas
+import json
 
 from button import Button
+from inputbox import InputBox
 
 logging.basicConfig(
     filename="LogFleetEngagement.log",
@@ -16,6 +18,9 @@ logging.basicConfig(
 ############################## PYGAME VARS #####################################
 pygame.init()
 
+CLOCK = pygame.time.Clock()
+CLOCK.tick(60)
+
 SCREEN_HEIGHT = 1280
 SCREEN_WIDTH = 720
 
@@ -27,7 +32,6 @@ BACKGROUND = pygame.image.load("./assets/Background.png")
 
 # var controls logic for initiating air related segments of combat
 # if false, air interactions not allowed and/or skipped
-
 AIR_ALLOWED = False
 
 NAVAL_ATTACK_TABLE = pandas.read_csv(
@@ -39,43 +43,32 @@ COMBAT_METHODS_LIST = [
     'NONE',
 ]
 
-COMBATANT_NATIONS_FLAGS_DICT = {
-        "GERMANY":"./assets/awaw_images/german/German_Marker_Flag.png",
-        "UNITED STATES":"./assets/awaw_images/usa/US_Marker_Flag.png",
-        "ITALY":"./assets/awaw_images/italy/Italy_Marker_Flag.png",
-        "JAPAN":"./assets/awaw_images/japan/Japan_Marker_Flag.png",
-        "COMMONWEALTH":"./assets/awaw_images/commonwealth/Brit_Marker_Flag.png",
-        "FRANCE":"./assets/awaw_images/france/France_Marker_Flag.png",
-        "RUSSIA":"./assets/awaw_images/russia/Russia_Marker_Flag.png",
-    }
-
-NATIONAL_ADJECTIVES_DICT = {
-        "GERMANY": "GERMAN",
-        "UNITED STATES": "AMERICAN",
-        "ITALY": "ITALIAN",
-        "JAPAN": "JAPANESE",
-        "COMMONWEALTH": "COMMONWEALTH",
-        "FRANCE": "FRENCH",
-        "RUSSIA": "RUSSIAN",
-    }
+with open("./tables/nations.json") as json_file:
+    NATIONS_DICT_LIST = json.load(json_file)
 
 ############################## GLOBAL METHODS ##################################
 
 def get_font(size):
     return pygame.font.Font("./assets/font.ttf", size)
          
-def choose_combatants(engagement):
+def choose_nations(engagement):
     """shows player nations they can choose to play as"""
+
+    logging.info("beginning choose_nations(engagement)")
 
     player_count = 0
     
     for combatant in engagement.combatants_list:
+
+        logging.info("player {} choosing combatant nation".format(combatant.short_designation_str))
 
         player_count = player_count + 1
 
         next_player = False
     
         while next_player is False:
+
+            logging.info("beginning choose_nations(engagement) while next_player is False for combatant {}".format(combatant.short_designation_str))
 
             pygame.display.set_caption("Choose Nations")
 
@@ -94,7 +87,7 @@ def choose_combatants(engagement):
             SCREEN.blit(MENU_TEXT, MENU_RECT)
         
             GERMANY_BUTTON      = Button(
-                image=pygame.image.load(COMBATANT_NATIONS_FLAGS_DICT['GERMANY']), 
+                image=pygame.image.load(NATIONS_DICT_LIST['Germany']['Flag']), 
                 pos=(350,420), text_input="", font=get_font(30), 
                 base_color="#d7fcd4", hovering_color="White")
             
@@ -104,7 +97,7 @@ def choose_combatants(engagement):
             SCREEN.blit(GERMANY_TEXT, GERMANY_TEXT_RECT)
             
             ITALY_BUTTON        = Button(
-                image=pygame.image.load(COMBATANT_NATIONS_FLAGS_DICT['ITALY']), 
+                image=pygame.image.load(NATIONS_DICT_LIST['Italy']['Flag']), 
                 pos=(650,420), text_input="", font=get_font(30), 
                 base_color="#d7fcd4", hovering_color="White")
             
@@ -114,7 +107,7 @@ def choose_combatants(engagement):
             SCREEN.blit(ITALY_TEXT, ITALY_TEXT_RECT)
                 
             JAPAN_BUTTON        = Button(
-                image=pygame.image.load(COMBATANT_NATIONS_FLAGS_DICT['JAPAN']), 
+                image=pygame.image.load(NATIONS_DICT_LIST['Japan']['Flag']), 
                 pos=(950,420), text_input="", font=get_font(30), 
                 base_color="#d7fcd4", hovering_color="White")
             
@@ -124,7 +117,7 @@ def choose_combatants(engagement):
             SCREEN.blit(JAPAN_TEXT, JAPAN_TEXT_RECT)
             
             COMMONWEALTH_BUTTON = Button(
-                image=pygame.image.load(COMBATANT_NATIONS_FLAGS_DICT['COMMONWEALTH']), 
+                image=pygame.image.load(NATIONS_DICT_LIST['Commonwealth']['Flag']), 
                 pos=(350,540), text_input="", font=get_font(30), 
                 base_color="#d7fcd4", hovering_color="White")
 
@@ -134,7 +127,7 @@ def choose_combatants(engagement):
             SCREEN.blit(COMMONWEALTH_TEXT, COMMONWEALTH_TEXT_RECT)        
             
             FRANCE_BUTTON       = Button(
-                image=pygame.image.load(COMBATANT_NATIONS_FLAGS_DICT['FRANCE']), 
+                image=pygame.image.load(NATIONS_DICT_LIST['France']['Flag']), 
                 pos=(650,540), text_input="", font=get_font(30), 
                 base_color="#f6fa00", hovering_color="White")
             FRANCE_TEXT = get_font(30).render("France", True, "White")
@@ -143,7 +136,7 @@ def choose_combatants(engagement):
             SCREEN.blit(FRANCE_TEXT, FRANCE_TEXT_RECT)        
             
             RUSSIA_BUTTON       = Button(
-                image=pygame.image.load(COMBATANT_NATIONS_FLAGS_DICT['RUSSIA']), 
+                image=pygame.image.load(NATIONS_DICT_LIST['Russia']['Flag']), 
                 pos=(950,540), text_input="", font=get_font(30), 
                 base_color="#d7fcd4", hovering_color="White")
             RUSSIA_TEXT = get_font(30).render("Russia", True, "White")
@@ -152,7 +145,7 @@ def choose_combatants(engagement):
             SCREEN.blit(RUSSIA_TEXT, RUSSIA_TEXT_RECT)        
             
             USA_BUTTON          = Button(
-                image=pygame.image.load(COMBATANT_NATIONS_FLAGS_DICT['UNITED STATES']), 
+                image=pygame.image.load(NATIONS_DICT_LIST['United States']['Flag']), 
                 pos=(650,300), text_input="", font=get_font(30), 
                 base_color="#d7fcd4", hovering_color="White")
 
@@ -168,7 +161,7 @@ def choose_combatants(engagement):
                 {'button': COMMONWEALTH_BUTTON, 'nation': 'Commonwealth'}, 
                 {'button': FRANCE_BUTTON,       'nation': 'France'}, 
                 {'button': RUSSIA_BUTTON,       'nation': 'Russia'}, 
-                {'button': USA_BUTTON,          'nation': 'USA'}
+                {'button': USA_BUTTON,          'nation': 'United States'}
             ]
 
             for button in buttons_list:
@@ -180,34 +173,44 @@ def choose_combatants(engagement):
             for event in events:
 
                 if event.type == pygame.QUIT:
+                    logging.info("event.type triggered pygame.QUIT")
                     pygame.quit()
                     sys.exit()
 
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     
                     for button in buttons_list:
+
                         if button['button'].checkForInput(MENU_MOUSE_POS):
-                            combatant.nationality = button['nation']
+                            
+                            logging.info("combatant {} choose {}".format(combatant.short_designation_str, button['nation']))
+                            logging.info("setting combatant {} nation_str, national_adjective_str, flag_str attributes".format(combatant.short_designation_str))
+                            
+                            combatant.nation_str = button['nation']
+                            combatant.national_adjective_str = NATIONS_DICT_LIST[combatant.nation_str]['Adjective']
+                            combatant.flag_str = NATIONS_DICT_LIST[combatant.nation_str]['Flag']
+                            
                             next_player = True
-                            #choose_combatants(engagement)
-                    # for button in button_list:
-                        # if button.checkForInput(MENU_MOUSE_POS):
-                            # log player's choice of combatant
 
             pygame.display.update()
 
 def choose_fleet_comp(engagement):
     """ask per player how many task forces in their fleet"""
 
-    player_count = 0
+    logging.info("setting choose_fleet_comp(engagement) combatant_count to zero")
+    combatant_count = 0
     
     for combatant in engagement.combatants_list:
 
-        player_count = player_count + 1
+        logging.info("beginning fleet_comp for combatant {}".format(combatant.short_designation_str))
 
-        next_player = False
+        combatant_count = combatant_count + 1
+
+        next_combatant = False
     
-        while next_player is False:
+        while next_combatant is False:
+
+            logging.info("beginning while next_combatant is False")
 
             pygame.display.set_caption("Determine number of Taskforces")
 
@@ -215,18 +218,50 @@ def choose_fleet_comp(engagement):
 
             MENU_MOUSE_POS = pygame.mouse.get_pos()
 
-            # place holder, should progamatically indicate the desired player
-            PLAYER_TITLE = get_font(50).render("Player {}...".format(player_count), True, "White")
+            logging.info("setting PLAYER_TITLE, PLAYER_TITLE_RECT")
+            
+            PLAYER_TITLE = get_font(50).render("Player {}...".format(combatant_count), True, "White")
             PLAYER_TITLE_RECT = PLAYER_TITLE.get_rect(center=(640,50))
 
-            MENU_TEXT = get_font(50).render("Choose your nation", True, "White")
-            MENU_RECT = MENU_TEXT.get_rect(center=(640,100))
+            logging.info("setting MENU_TEXT, MENU_RECT")
+            MENU_TEXT = get_font(30).render("Enter number of Taskforces", True, "White")
+            MENU_RECT = MENU_TEXT.get_rect(center=(640,150))
 
+            PLAYER_INPUT_RECT = InputBox(
+                510,370, 140, 32, get_font(30), 
+                pygame.Color(0,0,0), pygame.Color(255,255,0), text='')
+
+            logging.info("blit-ing MENU_TEXT, MENU_RECT")
             SCREEN.blit(PLAYER_TITLE, PLAYER_TITLE_RECT)
             SCREEN.blit(MENU_TEXT, MENU_RECT)
 
+            events = pygame.event.get()
+            
+            for event in events:
+
+                if event.type == pygame.QUIT:
+                    logging.info("event.type triggered pygame.QUIT")
+                    pygame.quit()
+                    sys.exit()
+
+                PLAYER_INPUT_RECT.handle_event(event)
+
+                #if event.type == pygame.MOUSEBUTTONDOWN:
+                #    pass
+            
+            PLAYER_INPUT_RECT.update()
+            PLAYER_INPUT_RECT.draw(SCREEN)
+        
+            logging.info("pygame.display.update()")
+            #pygame.display.update()
+            pygame.display.flip()
+
 def main_menu(engagement):
+
     while True:
+
+        logging.info("beginning main_menu() while True")
+
         pygame.display.set_caption("Menu")
         
         SCREEN.blit(BACKGROUND, (0,0))
@@ -244,7 +279,6 @@ def main_menu(engagement):
 
         SCREEN.blit(MENU_TEXT, MENU_RECT)
 
-        #for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
         for button in [PLAY_BUTTON, QUIT_BUTTON]:
             button.changeColor(MENU_MOUSE_POS)
             button.update(SCREEN)
@@ -258,13 +292,35 @@ def main_menu(engagement):
             if event.type == pygame.MOUSEBUTTONDOWN:
                 
                 if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
-                    choose_combatants(engagement)
+                    logging.info("MENU_MOUSE_POS MOUSBUTTONDOWN triggered choose_nations(engagement)")
+                    choose_nations(engagement)
+
+                    # if combatants have nations assigned, call fleet comp
+                    logging.info("Confirming all combatants have nation assigned")
+                    combatant_has_nation_count = 0
+
+                    for combatant in engagement.combatants_list:
+                        logging.info("Confirming combatant {} has nation assigned".format(combatant.short_designation_str))
+
+                        if combatant.nation_str in list(NATIONS_DICT_LIST.keys()):
+                            logging.info("Combatant {} has nation {} assigned, matches NATIONS_LIST".format(combatant.short_designation_str, combatant.nation_str))
+                            combatant_has_nation_count = combatant_has_nation_count + 1
+
+                        else:
+                            logging.info("Combatant {} does not have nation from NATIONS_LIST assigned.  Has {} assigned instead".format(combatant.short_designation_str, combatant.nation_str))
+                    
+                    if combatant_has_nation_count == len(engagement.combatants_list):
+                        logging.info("All combatants have proper nations assigned")
+                        logging.info("Executing choose_fleet_comp(engagement)")
+                        choose_fleet_comp(engagement)
                 
                 if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    logging.info("MENU_MOUSE_POS MOUSBUTTONDOWN triggered QUIT_BUTTON's pygame.quit()")
                     pygame.quit()
                     sys.exit()
 
-        pygame.display.update()
+        #pygame.display.update()
+        pygame.display.flip()
 
 ############################## CLASSES #########################################
 
@@ -272,8 +328,9 @@ class Combatant(object):
     """i.e. a player, or nation; handles combatant details"""
     def __init__(self, short_designation):
         self.short_designation_str = short_designation
-        self.nationality_str = None
+        self.nation_str = None
         self.national_adjective_str = None
+        self.flag_str = None
         self.fleet_composition_list = []
         self.round_search_results_int = 0
         self.search_rolls_list = []
@@ -302,7 +359,9 @@ if __name__ == "__main__":
     
     logging.info("FleetEngagement.py executed as __main__")
     
+    logging.info("Instantiating Fleet Engagement object")
     engagement = FleetEngagement()
     
     while True:
+        logging.info("calling main_menu")
         main_menu(engagement)
